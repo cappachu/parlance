18618e80e0230fb86eb55c7b8076a09290f58bea
cappachu <cappachu@gmail.com>
2015-06-11 11:57:44 -0400

    add setup.py, requirements.txt, LICENSE

---
+++ setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+import parlance
+
+setup(
+    name='parlance',
+    version=parlance.__version__,
+    description='chat with folks on your local area network',
+    author='Codanda B. Appachu',
+    url='https://github.com/cappachu/parlance',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'parlance = parlance.parlance:main',
+        ]
+    },
+    install_requires=[
+        'urwid',
+    ],
+)
